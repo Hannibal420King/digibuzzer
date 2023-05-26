1f221caa093ab234a5860b869198adcc7d7fdec5
Emmanuel ZIMMERT <ez@ladigitale.dev>
2023-05-26 07:08:23 +0200

    Update notification.vue

--- components/notification.vue
+++ components/notification.vue
@@ -7,6 +7,20 @@ export default {
 	props: {
 		notification: String
 	},
+	watch: {
+		notification: function (notification) {
+			const element = document.createElement('div')
+			const id = 'notification_' + Date.now().toString(36) + Math.random().toString(36).substring(2)
+			element.id = id
+			element.textContent = notification
+			element.classList.add('notification')
+			document.querySelector('#app').appendChild(element)
+			setTimeout(function () {
+				element.parentNode.removeChild(element)
+				this.$emit('fermer')
+			}.bind(this), 2500)
+		}
+	},
 	mounted () {
 		const element = document.createElement('div')
 		const id = 'notification_' + Date.now().toString(36) + Math.random().toString(36).substring(2)
