a048454746738e4327b10d2cd9aaf407052838ad
Emmanuel ZIMMERT <ez@ladigitale.dev>
2023-05-27 10:00:31 +0200

    Update _default.page.client.js

--- renderer/_default.page.client.js
+++ renderer/_default.page.client.js
@@ -4,7 +4,6 @@ import { createApp } from './app'
 
 async function render (pageContext) {
 	const { Page, pageProps } = pageContext
-	if (!Page) throw new Error('Client-side render() hook expects pageContext.Page to be defined')
 	const app = createApp(Page, pageProps, pageContext)
 	app.mount('#app')
 }
