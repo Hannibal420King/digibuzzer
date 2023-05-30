0cbbbb878c9842208643945ae9d1c2682c1a0f1d
Emmanuel ZIMMERT <ez@ladigitale.dev>
2023-05-30 18:11:42 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  apps: [{
-    name: 'Digibuzzer',
-    script: 'npm run server:prod'
-  }]
+	apps: [{
+    	name: 'Digibuzzer',
+    	script: 'npm -- run server:prod',
+		autorestart: true,
+		max_restarts: 20,
+		restart_delay: 1000
+	}]
 }
