e36c62ddc806cee815c2d681a05b19d78553180d
Emmanuel ZIMMERT <ez@ladigitale.dev>
2024-11-24 17:34:32 +0100

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,21 @@
 module.exports = {
 	apps: [{
     	name: 'Digibuzzer',
-    	script: 'npm -- run server:prod',
+    	script: 'server/index.js',
+		node_args: [
+			'--nouse-idle-notification',
+			'--expose-gc'
+		],
 		autorestart: true,
-		max_restarts: 20
+		max_restarts: 10,
+		// instances: 'max',
+		exec_interpreter: 'node',
+		// exec_mode: 'cluster',
+		env: {
+			'NODE_ENV': 'development'
+		},
+		env_production: {
+			'NODE_ENV': 'production'
+		}
 	}]
 }
